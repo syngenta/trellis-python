99279bfc81e181246bec00c955c104131484de69
ncgl-syngenta <nicholas.lawrence@sygenta.com>
2021-10-06 14:33:03 -0500

    move reconnection logic to sql connection decorator

--- syngenta_digital_dta/postgres/sql_connection.py
+++ syngenta_digital_dta/postgres/sql_connection.py
@@ -4,7 +4,17 @@ from syngenta_digital_dta.postgres.sql_connector import SQLConnector
 def sql_connection(func):
     __connections = {}
     def decorator(obj):
+
         if not __connections.get(obj.database):
             __connections[obj.database] = SQLConnector(obj)
-        return func(obj, __connections[obj.database])
+
+        try:
+            return func(obj, __connections[obj.database])
+        except Exception as err:
+            if 'already closed' in str(err):
+                __connections[obj.database] = SQLConnector(obj)
+                return func(obj, __connections[obj.database])
+            else:
+                raise err
+
     return decorator
