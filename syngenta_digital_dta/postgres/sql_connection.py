f3e9e57c266ad0b5323b2ff22732e4425d4055d1
ncgl-syngenta <nicholas.lawrence@sygenta.com>
2021-10-06 20:14:20 -0500

    test and update sql connector

--- syngenta_digital_dta/postgres/sql_connection.py
+++ syngenta_digital_dta/postgres/sql_connection.py
@@ -1,15 +1,16 @@
+import typing
+
 from syngenta_digital_dta.postgres.sql_connector import SQLConnector
 
 
-def sql_connection(func):
+def sql_connection(func: typing.Callable):
     __connections = {}
-    def decorator(obj):
 
+    def decorator(obj):
         if __connections.get(obj.database):
-            if obj.connection:
-                if obj.connection.closed or obj.cursor.closed:
-                    __connections[obj.database] = SQLConnector(obj)
-                return func(obj, __connections[obj.database])
+            if obj.connection.closed or obj.cursor.closed:
+                __connections[obj.database] = SQLConnector(obj)
+            return func(obj, __connections[obj.database])
 
         __connections[obj.database] = SQLConnector(obj)
         return func(obj, __connections[obj.database])
