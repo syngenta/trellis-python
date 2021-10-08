1e6bd17566334f9a61681974043a0b1f7f99a73b
ncgl-syngenta <87993251+ncgl-syngenta@users.noreply.github.com>
2021-10-08 13:39:38 -0500

    Connection caching (#78)
    * test and update sql connector
    
    * lint
    
    * add test
    
    * pr review
    
    * pr review
    
    Co-authored-by: ncgl-syngenta <nicholas.lawrence@sygenta.com>

--- syngenta_digital_dta/postgres/sql_connection.py
+++ syngenta_digital_dta/postgres/sql_connection.py
@@ -1,10 +1,18 @@
+import typing
+
 from syngenta_digital_dta.postgres.sql_connector import SQLConnector
 
 
-def sql_connection(func):
+def sql_connection(func: typing.Callable) -> typing.Callable:
     __connections = {}
-    def decorator(obj):
-        if not __connections.get(obj.database):
-            __connections[obj.database] = SQLConnector(obj)
+
+    def decorator(obj: typing.Union["PostgresAdapter", "RedshiftAdapter"]):
+
+        # reuse the existing connection if it isn't closed
+        if __connections.get(obj.database) and __connections[obj.database].connection and not __connections[obj.database].connection.closed:
+            return func(obj, __connections[obj.database])
+
+        __connections[obj.database] = SQLConnector(obj)
         return func(obj, __connections[obj.database])
+
     return decorator
