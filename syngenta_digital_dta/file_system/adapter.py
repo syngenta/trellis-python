c912b1752cfe063ea087d649b9643aced21c183a
Mario Macedo <mario.macedo@syngenta.com>
2022-04-08 13:28:49 -0500

    updating s3 method

--- syngenta_digital_dta/file_system/adapter.py
+++ syngenta_digital_dta/file_system/adapter.py
@@ -5,6 +5,7 @@ import jsonpickle
 import requests
 
 from syngenta_digital_dta.common.base_adapter import BaseAdapter
+from syngenta_digital_dta import S3Adapter
 
 
 class FileSystemAdapter(BaseAdapter):
@@ -16,25 +17,21 @@ class FileSystemAdapter(BaseAdapter):
     def create(self, **kwargs):
         body = self.__set_body(**kwargs)
         destination_path = kwargs['destination_path']
-        publish = kwargs.get('publish', True)
 
         self.__create_destination_directory(destination_path)
 
         with open(destination_path, 'wb') as file:
             file.write(body)
 
-        if publish:
+        if kwargs.get('publish', True):
             data = {'file_path': destination_path}
             super().publish('create', data)
 
     def read(self, **kwargs) -> bytes:
-        file_path = kwargs['file_path']
-        json = kwargs.get('json', False)
-
-        with open(file_path, 'rb') as file:
+        with open(kwargs['file_path'], 'rb') as file:
             body = file.read()
 
-        if json:
+        if kwargs.get('json', False):
             return jsonpickle.decode(body)
         return body
 
@@ -48,9 +45,8 @@ class FileSystemAdapter(BaseAdapter):
 
     def delete(self, **kwargs):
         path = kwargs['path']
-        recursive = kwargs.get('recursive', False)
 
-        if os.path.isdir(path) and recursive:
+        if os.path.isdir(path) and kwargs.get('recursive', False):
             shutil.rmtree(path)
         elif os.path.isdir(path):
             raise DeleteDirectoryException(f'Set recursive to True to delete contents in folder at {path}')
@@ -58,27 +54,41 @@ class FileSystemAdapter(BaseAdapter):
             os.remove(path)
 
     def get_age(self, **kwargs) -> float:
-        path = kwargs['path']
-        return os.stat(path).st_ctime
+        return os.stat(kwargs['path']).st_ctime
 
     def __set_body(self, **kwargs) -> bytes:
         if kwargs.get('file_object'):
             return kwargs['file_object']
         if kwargs.get('file_path'):
             return self.read(file_path=kwargs['file_path'])
-        if kwargs.get('http_link') or kwargs.get('s3_link'):
+        if kwargs.get('http_link'):
             return self.__download_file(**kwargs)
-        raise FileObjectExpception('No file_object, file_path, http_link, or s3_link provided')
+        if kwargs.get('s3_path'):
+            return self.__download_from_s3(**kwargs)
+        raise FileObjectExpception('No file_object, file_path, http_link, or s3_path provided')
 
     def __create_destination_directory(self, destination_path):
         os.makedirs(os.path.dirname(destination_path), exist_ok=True)
 
     def __download_file(self, **kwargs) -> bytes:
-        link = kwargs.get('http_link', kwargs['s3_link'])
+        link = kwargs.get['http_link']
         headers = kwargs.get('headers')
 
         response = requests.request('GET', link, headers=headers)
         return response.content
+
+    def __init_s3_adapter(self, **kwargs):
+        return S3Adapter(
+            region=kwargs.get('region'),
+            endpoint=kwargs.get('s3_enpoint'),
+            bucket=kwargs.get('bucket'),
+        )
+
+    def __download_from_s3(self, **kwargs) -> bytes:
+        s3_adapter = self.__init_s3_adapter(**kwargs)
+        kwargs['decode'] = False
+        results = s3_adapter.get(**kwargs)
+        return results['Body'].read()
 
 
 class DeleteDirectoryException(Exception):
