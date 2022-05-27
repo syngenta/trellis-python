bbd5823a366e4a1d2d231c41461b02f527d0f971
Mario Macedo <mario.macedo@syngenta.com>
2022-05-27 15:34:45 -0500

    maybe fixing unit test

--- tests/syngenta_digital_dta/s3/test_s3.py
+++ tests/syngenta_digital_dta/s3/test_s3.py
@@ -17,8 +17,8 @@ class S3AdapterTest(unittest.TestCase):
         warnings.simplefilter('ignore', ResourceWarning)
         self.maxDiff = None
         self.bucket = 'unit-test'
-        # self.endpoint = 'http://localhost:4566'
-        self.endpoint = 'http://localhost:4000'
+        self.endpoint = 'http://localhost:4566'
+        # self.endpoint = 'http://localhost:4000'
         self.file = open('./tests/mock/example.json')
         self.__create_unit_test_bucket()
         self.adapter = syngenta_digital_dta.adapter(
@@ -39,7 +39,15 @@ class S3AdapterTest(unittest.TestCase):
         )
         # s3_client = boto3.client(service_name='s3', aws_access_key_id='test-access-key', aws_secret_access_key='test-secret-key', endpoint_url=self.endpoint)
             # s3_client = boto3.client('s3', endpoint_url=self.endpoint, region='us-east-2')
+        response = s3_client.list_buckets()
+        print('Existing buckets:')
+        for bucket in response['Buckets']:
+            print(f'  {bucket["Name"]}')
         s3_client.create_bucket(Bucket=self.bucket)
+        response = s3_client.list_buckets()
+        print('Existing buckets after creation:')
+        for bucket in response['Buckets']:
+            print(f'  {bucket["Name"]}')
         # except:
             # pass
 
