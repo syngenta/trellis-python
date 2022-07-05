6d10f047771da2a89443f03c16e8eefb435e2801
Paul Cruse III <paul.cruse@syngenta.com>
2022-07-05 14:17:30 -0500

    changing quotes

--- tests/syngenta_digital_dta/postgres/test_json_formatting.py
+++ tests/syngenta_digital_dta/postgres/test_json_formatting.py
@@ -129,10 +129,10 @@ class TestJsonFormatting(unittest.TestCase):
             json_column_map=json_columns
         )
 
-        expected = """WITH _json_cte AS (SELECT "{}"::json AS _json)
+        expected = '''WITH _json_cte AS (SELECT "{}"::json AS _json)
         INSERT INTO my_table (guuid, created_at, updated_at, time, machine, secID)
         SELECT generate_uuid_v4() AS guuid, now() AS created_at, now() AS updated_at, _jsondict -> "properties" ->> "Time" AS time, _jsondict -> "properties" ->> "Machine" AS machine, _jsondict -> "properties" ->> "SecID" AS secID
-        FROM (SELECT json_array_elements(_json->"feature") AS _jsondict FROM _json_cte)x"""
+        FROM (SELECT json_array_elements(_json->"feature") AS _jsondict FROM _json_cte)x;'''
 
         self.assertEqual(expected, actual)
 
@@ -163,10 +163,10 @@ class TestJsonFormatting(unittest.TestCase):
             }
         )
 
-        expected = """WITH _json_cte AS (SELECT "{}"::json AS _json)
+        expected = '''WITH _json_cte AS (SELECT "{}"::json AS _json)
         INSERT INTO my_table (guuid, created_at, updated_at, time, machine, secID)
         SELECT generate_uuid_v4() AS guuid, now() AS created_at, now() AS updated_at, _jsondict -> "properties" ->> "Time" AS time, _jsondict -> "properties" ->> "Machine" AS machine, cast(_jsondict -> "properties" ->> "SecID" as varchar) as secID
-        FROM (SELECT json_array_elements(_json->"feature") AS _jsondict FROM _json_cte)x"""
+        FROM (SELECT json_array_elements(_json->"feature") AS _jsondict FROM _json_cte)x'''
 
         self.assertEqual(expected, actual)
 
