76e4671d3e46c748fdf9c732075d54c40aaf7cc2
ncgl-syngenta <nicholas.lawrence@syngenta.com>
2022-11-08 10:50:57 -0600

    pr review

--- tests/syngenta_digital_dta/elasticsearch/test_es_connector.py
+++ tests/syngenta_digital_dta/elasticsearch/test_es_connector.py
@@ -1,10 +1,11 @@
-import uuid
 import unittest
 import warnings
-import json
+from collections import namedtuple
+from unittest import mock
 
 from syngenta_digital_dta.elasticsearch.es_connector import ESConnector
 from tests.syngenta_digital_dta.elasticsearch.mocks import MockESAdapter
+
 
 class ESConnectorTest(unittest.TestCase):
 
@@ -23,7 +24,38 @@ class ESConnectorTest(unittest.TestCase):
         self.assertEqual(connector.port, mock_adapter.port)
 
     def test_class_port_user_pass(self):
-        mock_adapter = MockESAdapter(endpoint='dev.aws.com', user='root', password='root', authentication='user-password')
+        mock_adapter = MockESAdapter(endpoint='dev.aws.com', user='root', password='root',
+                                     authentication='user-password')
         connector = ESConnector(mock_adapter)
         self.assertEqual(connector.user, mock_adapter.user)
         self.assertEqual(connector.password, mock_adapter.password)
+
+    @mock.patch('syngenta_digital_dta.elasticsearch.es_connector.Elasticsearch')
+    def test_elasticsearch_constructor(self, mock_elasticsearch):
+        cls = namedtuple('cls', ['endpoint', 'port', 'authentication', 'user', 'password'])
+
+        kwargs = cls(
+            endpoint='endpoint',
+            port=1234,
+            authentication='user-password',
+            user='user',
+            password='password'
+        )
+
+        esc = ESConnector(kwargs)
+        esc.connect()
+
+        mock_elasticsearch.assert_called_with(
+            hosts=[
+                {
+                    'host': 'endpoint',
+                    'port': 1234
+                }
+            ],
+            use_ssl=True,
+            verify_certs=True,
+            connection_class=mock.ANY,
+            timeout=30,
+            request_timeout=30,
+            http_auth=('user', 'pasword')
+        )
